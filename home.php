06488483e4af4277e30dedcc0c246d30fa97c1a4
alex-vb <vietbinh.dn@gmail.com>
2017-01-14 14:18:12 +0700

    home
    page home

--- home.php
+++ home.php
@@ -44,17 +44,113 @@
 
 
 <div class="container">
-    <div class="row">
+    <div class="row new-container">
         <div class="col-md-8">
-            Gioi thieu
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span>
+                        Giới thiệu
+                    </span>
+                </div>
+                <div class="panel-body">
+                    <img style="float: left" src="img/about.jpg"/>
+                    <div class="about-description">
+                        <p>
+                            <span style="font-family: arial, helvetica, sans-serif;"><span style="color: #bf1e2e;"><strong><span style="font-size: 12pt;">Headquartered in&nbsp;</span></strong></span></span><span style="font-family: arial,helvetica,sans-serif;"><strong><span style="font-size: 9pt;">Guangzhou, Folangsi is well recognized as China’s largest comprehensive service provider for forklift sales, rentals, repairs &amp; parts development and supply, and a global one-stop service provider specializing in forklift parts development and sales. Founded in Match 2001, Folangsi, through many years of expansion</span></strong></span><span style="font-family: arial, helvetica, sans-serif;"><span style="color: #bf1e2e;"><strong><span style="font-size: 9pt;"><span style="color: #000000;">. ...[<a href="http://www.folangsiforklift.com/comcontent_detail/&amp;FrontComContent_list01-1367914459712ContId=17&amp;comContentId=17.html">View Details</a>]</span></span></strong></span></span></p>
+                        <p class="read-more" style="text-align: right;color: red">Xem thêm</p>
+                    </div>
+
+                </div>
+            </div>
         </div>
         <div class="col-md-4">
-            Tin tuc
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span>
+                        Tin tức
+                    </span>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-news">
+                        <li>
+                            <a href="#">Tiêu đề bài viết số 1</a>
+
+                        </li>
+                        <li>
+                            <a href="#">Tiêu đề bài viết số 1</a>
+
+                        </li>
+                        <li>
+                            <a href="#">Tiêu đề bài viết số 1</a>
+
+                        </li>
+                        <li>
+                            <a href="#">Tiêu đề bài viết số 1</a>
+
+                        </li>
+                        <li>
+                            <a href="#">Tiêu đề bài viết số 1</a>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">
-            San pham
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span>
+                        Sản phẩm
+                    </span>
+                </div>
+                <div class="panel-body">
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+                    <div class="col-md-3 box-product">
+                        <a href="#">
+                            <img src="http://www.folangsiforklift.com/imageRepository/a10bdfa0-d28b-4e46-87ed-c7ddd12fb211.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                        </a>
+                        <a class="name-product" href="#">Sản phảm</a>
+                        <p class="code-product">CH-003-9999</p>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     </div>
