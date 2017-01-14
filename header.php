dded1f91de4aa9b289cf6aab08dd07a4aeab8157
alex-vb <vietbinh.dn@gmail.com>
2017-01-14 10:20:45 +0700

    header
    update

--- header.php
+++ header.php
@@ -1,24 +1,44 @@
-<div id="header" class="row">
-    <div id="flags" class="col-md-12">
-        <ul>
-            <li>Chinese</li>
-            <li>English</li>
-            <li>Vietnamese</li>
-        </ul>
+<div id="flags" class="container-fluid">
+    <div class="">
+        <div  class="col-md-12">
+            <ul>
+                <li>Chinese</li>
+                <li>English</li>
+                <li>Vietnamese</li>
+            </ul>
+        </div>
     </div>
-    <!--End flags-->
-    <div id="logo" class="col-md-6">
-        Logo
+
+</div>
+<div id="header" class="container">
+    <div class="row">
+        <!--End flags-->
+        <div id="logo" class="col-md-6">
+            <a href="#">
+                <img src="img/logo.jpg">
+            </a>
+        </div>
+        <!--End logo-->
+
+        <div id="search" class="col-md-6">
+            <div id="custom-search-input" class="col-md-10">
+                <div class="input-group">
+                    <input type="text" class="form-control input-sm" placeholder="Tìm kiếm" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
-    <!--End logo-->
-    <div id="search" class="col-md-6">
-        Search
-    </div>
+
 </div>
 <!--End header-->
 
-<div class="row">
-    <div class="main-menu col-md-12">
+<div class="container-fluid">
+    <div class="main-menu">
         <nav class="navbar navbar-default" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
