524aeccf736bf412eb3ad79a3ce4e9028ae9bd1b
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-01-15 08:45:53 +0700

    product

--- product.php
+++ product.php
@@ -1,7 +1,74 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ThaoGiang
- * Date: 1/15/2017
- * Time: 8:22 AM
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dien may xanh</title>
+    <link href="css/bootstrap.min.css" media="all" rel="stylesheet">
+    <link href="css/style.css" media="all" rel="stylesheet">
+    <script src="js/jquery/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include_once("header.php") ?>
+<div class="feedback_bg">
+    <div id="main-content" class="container">
+        <div class="row content_feedback">
+            <div class="sidebar col-md-3">
+                <div class="panel">
+                    <div class="panel-heading">
+                        Product
+                    </div>
+                    <!--                    End heading-->
+                    <div class="panel-body">
+                        <ul>
+                            <li>Warehouse Equipments</li>
+                            <li>Forklift</li>
+                            <li>Chassis Parts & others</li>
+                            <li>Brake Parts</li>
+                            <li>Cooling Parts</li>
+                            <li>Drive Parts</li>
+                            <li>Engine</li>
+                            <li>Filters</li>
+                            <li>Standard Parts</li>
+                            <li>Hydraulic Series</li>
+                            <li>Wheel Rim & Tire</li>
+                            <li>Electrical Parts</li>
+                            <li>Mast Parts</li>
+                            <li>Electric Series</li>
+                            <li>Mast Parts</li>
+                            <li>Electric Series</li>
+                            <li>Appliance Series</li>
+                            <li>Steering Series</li>
+                            <li>Transmission Series</li>
+                            <li>Attachments</li>
+                            <li>Type Press</li>
+                            <li>Electric Vehicles</li>
+                            <li>Racking & Shelving</li>
+
+
+
+                        </ul>
+                    </div>
+<!--                    End body-->
+
+                </div>
+<!--                End panel-->
+
+            </div>
+<!--            End side bar-->
+            <div class="col-md-9">
+                <div class="panel">
+
+                </div>
+<!--                End panel body Product-->
+            </div>
+        </div>
+<!--        End Content Product-->
+
+    </div>
+</div>
+<!--End Content-->
+
+<?php include_once("footer.php") ?>
+
+</body>
+</html>
