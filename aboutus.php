52cd85d396821fc7bad0e602ea1a117abd77db24
alex-vb <vietbinh.dn@gmail.com>
2017-01-15 10:00:25 +0700

    about
    content

--- aboutus.php
+++ aboutus.php
@@ -46,8 +46,21 @@
                     <div class="panel-heading">Panel Heading</div>
                     <div class="panel-body">
                         <div id='container'>
-                           
 
+                            <div class="content" style="line-height: 30px;">
+                                <div class="describe htmledit">
+                                    <div class="FrontComContent_detail01-1367915480304_htmlbreak" id="FrontComContent_detail01-1367915480304_cont_1" style="display: block;"><p style="margin-top: 0em; text-indent: 2em; margin-bottom: 0em; letter-spacing: 0em;">
+                                            <span style="font-size:10pt;"><span style="font-family: arial, helvetica, sans-serif; "><span style="color: rgb(0, 0, 0); "><img alt="" src="http://www.folangsiforklift.com/imageRepository/87d9ae68-9677-408d-94d6-f723f0f2712d.jpg" style="padding-right: 20px; float: left;"></span>&nbsp;As Folangsi has gone through 12 years, we started from a humble beginning towards large-scale, condensing Folangsi people’s arduous sweat step by step, carrying their infinite dreams. As the professional and the unprofessional exist in this industry, we, from a pioneer, innovator to leader, insist on forklift project as core business, leading new trend in China’s forklift service industry by professionalization, chain-store operation, Scalization and Internationalization.</span></span></p>
+                                        <p style="margin-top: 0em; text-indent: 2em; margin-bottom: 0em; letter-spacing: 0em;">
+                                            &nbsp;</p>
+                                        <p style="text-indent:24.0pt;">
+                                            <span style="font-size:10pt;"><span style="font-family: arial, helvetica, sans-serif; ">For a decade, we insist on ‘Promoting the construction of China’s and global logistics system &amp; Making tremendous positive impact on it’ as the Enterprise mission, ‘Talents, Management and Goals’ as the Management aim. Folangsi, continually striving for innovation, has 4 major business units by now: Forklift sales, Parts sales, logistics-related equipment sales, rentals and used forklifts sales.</span></span></p>
+                                        <p style="text-indent:24.0pt;">
+                                            &nbsp;</p>
+                                        <p style="text-indent:24.0pt;">
+                                            <span style="font-size:10pt;"><span style="font-family: arial, helvetica, sans-serif; ">&nbsp;‘Idle boast the strong pass is a wall of iron, With firm strides we are crossing its summit’. Looking ahead to the future, Folangsi surely continues to create the platform by efforts and wisdom to be the most outstanding forklift service enterprise in Chinaand even in the world,maximizing its values by growing stronger &amp; giving back to society.</span></span></p></div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
