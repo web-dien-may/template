51fc41d9c1db914ecffeddeb16eafadd5ebdd30f
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-01-15 09:49:47 +0700

    about us

---
+++ aboutus.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dien may xanh</title>
+    <link href="css/bootstrap.min.css" media="all" rel="stylesheet">
+    <link href="css/style.css" media="all" rel="stylesheet">
+    <script src="js/jquery/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include_once("header.php") ?>
+<div class="feedback_bg">
+    <div id="main-content" class="container">
+        <div class="row content_feedback">
+            <div class="sidebar col-md-3">
+                <div class="panel">
+                    <div class="panel-heading">
+                        About us
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li><span class="glyphicon glyphicon-chevron-right"></span>Honor</li>
+                            <li><span class="glyphicon glyphicon-chevron-right"></span>History</li>
+                            <li><span class="glyphicon glyphicon-chevron-right"></span>Company Profile</li>
+                            <li><span class="glyphicon glyphicon-chevron-right"></span>Message From Chairman</li>
+                        </ul>
+
+
+                    </div>
+                </div>
+
+                <div class="box-ads">
+                    <img src="img/member_center.jpg" alt="Member Center Feedback">
+                </div>
+                <!--End Feedback Member-->
+                <div class="box-ads">
+                    <img src="img/Contactus_feedback.jpg" alt="Contact Us FeedBack">
+                </div>
+                <!--End Contact us Member-->
+
+
+            </div>
+
+            <div class="col-md-9 feedback-content">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Panel Heading</div>
+                    <div class="panel-body">
+                        <div id='container'>
+                           
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+
+    </div>
+</div>
+<!--End Content-->
+
+<?php include_once("footer.php") ?>
+
+</body>
+</html>
